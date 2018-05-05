bba74a7a1108580c3eac24d04c182b8318e28dcf
row10x64-vm\robispo <rabelobispo@hotmail.com>
2018-05-05 14:23:33 -0400

    First commit

---
+++ DatingApp.SPA/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ValueComponent,
+    NavComponent
+],
+  imports: [
+    BrowserModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ README.md
@@ -0,0 +1,8 @@
+# Angular Examples
+
+From scratch I will learn to use **Angular** throught baby steps, and like a baby i hope that you help me.
+
+## Step One:
+
+* Buy a course by **Udemy** call **Build an app with ASPNET Core and Angular from scratch**
+* Finish the course
